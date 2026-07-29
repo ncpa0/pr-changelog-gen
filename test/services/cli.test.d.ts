ecbd93304c7a03993b2129f5cf6b7ea02ea80670
ncpa0cpl <szymonb21@gmail.com>
2026-07-29 18:50:47 +0200

    lint: fix fmt

--- test/services/cli.test.d.ts
+++ test/services/cli.test.d.ts
@@ -3,9 +3,13 @@ import type { PullRequest, SemverNumber } from "../../src/shared-types";
 import { Repo } from "../../src/utils/repo";
 import type { LoggerMockParams } from "../shared";
 export type CliFactoryParams = {
-    ensureCleanLocalGitState: (githubRepo: Repo) => Promise<void>;
-    getMergedPullRequests: (githubRepo: Repo) => Promise<Array<PullRequest>>;
-    createChangelog: (newVersionNumber: SemverNumber, pullRequests: Array<PullRequest>, githubRepo: Repo) => Promise<string>;
-    prependFile: (filePath: string, content: string) => Promise<void>;
-    config: ConfigFacade;
+  ensureCleanLocalGitState: (githubRepo: Repo) => Promise<void>;
+  getMergedPullRequests: (githubRepo: Repo) => Promise<Array<PullRequest>>;
+  createChangelog: (
+    newVersionNumber: SemverNumber,
+    pullRequests: Array<PullRequest>,
+    githubRepo: Repo,
+  ) => Promise<string>;
+  prependFile: (filePath: string, content: string) => Promise<void>;
+  config: ConfigFacade;
 } & LoggerMockParams;
--- test/shared.d.ts
+++ test/shared.d.ts
@@ -6,23 +6,24 @@ import type { Logger } from "../src/modules/logger";
 import type { PullResponse } from "../src/services/pull-request-resolver";
 type DeepPartial<T> = T extends object ? {
     [P in keyof T]?: DeepPartial<T[P]>;
-} : T;
+  }
+  : T;
 type AsInterface<C> = {
-    [P in keyof C]: C[P];
+  [P in keyof C]: C[P];
 };
 export declare const mockConfig: (overrides?: Config, overrideDefaults?: Config) => ConfigFacade;
 export declare const mockGithubClient: (overrides?: DeepPartial<InstanceType<typeof Octokit>>) => {
-    request: Mock<() => Promise<PullResponse>>;
+  request: Mock<() => Promise<PullResponse>>;
 };
 export type LoggerMockParams = {
-    logWrite?: Mock<(v: string) => void>;
-    logWarn?: Mock<(v: string) => void>;
-    logError?: Mock<(v: string) => void>;
+  logWrite?: Mock<(v: string) => void>;
+  logWarn?: Mock<(v: string) => void>;
+  logError?: Mock<(v: string) => void>;
 };
 export declare class LoggerMock implements AsInterface<Logger> {
-    constructor(params?: LoggerMockParams);
-    warn: Mock<(v: string) => void>;
-    write: Mock<(v: string) => void>;
-    error: Mock<(v: string) => void>;
+  constructor(params?: LoggerMockParams);
+  warn: Mock<(v: string) => void>;
+  write: Mock<(v: string) => void>;
+  error: Mock<(v: string) => void>;
 }
 export {};
