31b3190d2e553d7af6ef209ca159cdbdc3b8e5f0
ncpa0cpl <szymonb21@gmail.com>
2026-07-29 18:50:11 +0200

    chore: update nodepack

---
+++ test/services/cli.test.d.ts
@@ -0,0 +1,11 @@
+import { ConfigFacade } from "../../src/modules/config";
+import type { PullRequest, SemverNumber } from "../../src/shared-types";
+import { Repo } from "../../src/utils/repo";
+import type { LoggerMockParams } from "../shared";
+export type CliFactoryParams = {
+    ensureCleanLocalGitState: (githubRepo: Repo) => Promise<void>;
+    getMergedPullRequests: (githubRepo: Repo) => Promise<Array<PullRequest>>;
+    createChangelog: (newVersionNumber: SemverNumber, pullRequests: Array<PullRequest>, githubRepo: Repo) => Promise<string>;
+    prependFile: (filePath: string, content: string) => Promise<void>;
+    config: ConfigFacade;
+} & LoggerMockParams;
---
+++ test/shared.d.ts
@@ -0,0 +1,28 @@
+import type { Octokit } from "@octokit/rest";
+import { Mock } from "vitest";
+import type { Config } from "../src/modules/config";
+import { ConfigFacade } from "../src/modules/config";
+import type { Logger } from "../src/modules/logger";
+import type { PullResponse } from "../src/services/pull-request-resolver";
+type DeepPartial<T> = T extends object ? {
+    [P in keyof T]?: DeepPartial<T[P]>;
+} : T;
+type AsInterface<C> = {
+    [P in keyof C]: C[P];
+};
+export declare const mockConfig: (overrides?: Config, overrideDefaults?: Config) => ConfigFacade;
+export declare const mockGithubClient: (overrides?: DeepPartial<InstanceType<typeof Octokit>>) => {
+    request: Mock<() => Promise<PullResponse>>;
+};
+export type LoggerMockParams = {
+    logWrite?: Mock<(v: string) => void>;
+    logWarn?: Mock<(v: string) => void>;
+    logError?: Mock<(v: string) => void>;
+};
+export declare class LoggerMock implements AsInterface<Logger> {
+    constructor(params?: LoggerMockParams);
+    warn: Mock<(v: string) => void>;
+    write: Mock<(v: string) => void>;
+    error: Mock<(v: string) => void>;
+}
+export {};
